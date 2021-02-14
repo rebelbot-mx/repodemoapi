9a1a834025bd65f603e333247157e7c6601c2a8d
marcos cabrera <dev.marcoscabrera@gmail.com>
2021-02-14 11:17:45 -0600

    cambios en variables conexion base de datos

--- apidatos/clsConexionDB.php
+++ apidatos/clsConexionDB.php
@@ -7,15 +7,15 @@
     if($EN_PRODUCCION=='SI') {
 
    // DB::$host='';
-    DB::$user = 'mcabrera';
-    DB::$password = '2478';
-    DB::$dbName = 'apialdeas';
-    }else {
-
     DB::$host='mysqlApiAldeas.mysql.database.azure.com';
     DB::$user = 'phpappuser@mysqlApiAldeas';
     DB::$password = 'mysqlazure2017';
-    DB::$dbName = 'sampledb';       
+    DB::$dbName = 'sampledb';  
+    }else {
+  DB::$user = 'mcabrera';
+    DB::$password = '2478';
+    DB::$dbName = 'apialdeas';
+     
     }
 
 
