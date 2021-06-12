d539f4abde1d168246ddfd81d7f9ff3c4cae196d
marcos cabrera <dev.marcoscabrera@gmail.com>
2021-06-12 09:50:56 -0500

    camobios

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ $app->get('/',function(Request $request , Response $response):Response{
      
    
 
-    $response->getBody()->write("api activa version 1.1.1");
+    $response->getBody()->write("api activa version 1.1.2");
 
     return $response;
 
