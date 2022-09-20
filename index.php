9617cfc1ba6c7547734c2f5d8475a3a75c3500c2
marcos cabrera <dev.marcoscabrera@gmail.com>
2022-09-20 11:04:05 -0500

    cambio

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ $app->get('/',function(Request $request , Response $response):Response{
    
      
 
-    $response->getBody()->write("api activa version 1.1.4 produccion 21 de octubre");
+    $response->getBody()->write("api activa version 1.1.5 produccion 20 de septiembre");
 
     return $response;
 
